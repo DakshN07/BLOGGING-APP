5ccd5f97073af112d95749718d2348d9c4c535c5
Your Name <youremail@example.com>
2025-04-15 16:56:12 +0530

    Added Multer middleware to handle file uploads

---
+++ multer.js
@@ -0,0 +1,15 @@
+// multer.js
+const multer = require('multer');
+
+const upload = multer({
+  dest: '/tmp/',
+  limits: { fileSize: 1000000 },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'));
+    }
+    cb(undefined, true);
+  }
+});
+
+module.exports = upload;
